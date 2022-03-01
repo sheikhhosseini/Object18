00fb5a62d0349bbf6822b1078e2fae2df8d76a6d
Saeed-ssd <saeed779911@gmail.com>
2022-03-01 16:38:46 +0330

    #23 Add Repository To core

---
+++ Core/Repository/GenericRepository.cs
@@ -0,0 +1,76 @@
+﻿using Data.Context;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Repository;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity :BaseModel
+{
+    private readonly MainDbContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+    public GenericRepository(MainDbContext context)
+    {
+        _context = context;
+        _dbSet = this._context.Set<TEntity>();
+    }
+
+    public IQueryable<TEntity> GetEntitiesQuery()
+    {
+        return _dbSet.AsQueryable();
+    }
+
+    public async Task<TEntity> GetEntityById(long entityId)
+    {
+        return await _dbSet.SingleOrDefaultAsync(e => e.Id == entityId);
+    }
+
+    public async Task<TEntity> GetEntityByIdAsNoTracking(long entityId)
+    {
+        return await _dbSet.AsNoTracking().SingleOrDefaultAsync(e => e.Id == entityId);
+    }
+
+    public async Task AddEntity(TEntity entity)
+    {
+        entity.CreateDate = DateTime.Now;
+        entity.LastUpdateDate = entity.CreateDate;
+        await _dbSet.AddAsync(entity);
+    }
+
+    public void UpdateEntity(TEntity entity)
+    {
+        entity.LastUpdateDate = DateTime.Now;
+        _dbSet.Update(entity);
+    }
+
+    public void SoftRemoveEntity(TEntity entity)
+    {
+        entity.IsDelete = true;
+        UpdateEntity(entity);
+    }
+    public async Task SoftRemoveEntityById(long entityId)
+    {
+        var entity = await GetEntityById(entityId);
+        SoftRemoveEntity(entity);
+    }
+
+    public void HardRemoveEntity(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+    }
+
+    public async Task HardRemoveEntityById(long entityId)
+    {
+        var entity = await GetEntityById(entityId);
+        HardRemoveEntity(entity);
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context?.Dispose();
+    }
+}
---
+++ Core/Repository/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using Data.Models;
+
+namespace Core.Repository;
+
+public interface IGenericRepository<TEntity> : IDisposable where TEntity:BaseModel
+{
+    IQueryable<TEntity> GetEntitiesQuery();
+    Task<TEntity> GetEntityById(long entityId);
+    Task<TEntity> GetEntityByIdAsNoTracking(long entityId);
+    Task AddEntity(TEntity entity);
+    void UpdateEntity(TEntity entity);
+    void SoftRemoveEntity(TEntity entity);
+    Task SoftRemoveEntityById(long entityId);
+    void HardRemoveEntity(TEntity entity);
+    Task HardRemoveEntityById(long entityId);
+    Task SaveChangesAsync();
+}
