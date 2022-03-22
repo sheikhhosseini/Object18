2b7e01f02c3259042879abf289780cf21a88660c
mssheikh1377 <mssheikh1377@gmail.com>
2022-03-22 18:24:18 +0430

    Remove Test Module

--- Ioc/DependencyContainer.cs
+++ Ioc/DependencyContainer.cs
@@ -1,8 +1,6 @@
 ﻿using Core.Modules.Account.Dtos;
 using Core.Modules.Account.Services;
 using Microsoft.Extensions.DependencyInjection;
-using Core.Modules.TestModule.Dtos;
-using Core.Modules.TestModule.Services;
 using Core.Repository;
 using Core.Shared.Email;
 
@@ -13,7 +11,6 @@ public class DependencyContainer
     public static void RegisterServices(IServiceCollection service)
     {
         service.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-        service.AddScoped<ITestService, TestService>();
         service.AddScoped<IAccountService, AccountService>();
 
         #region Email
@@ -22,7 +19,6 @@ public class DependencyContainer
         #endregion
 
         #region AutoMapper Configs
-        service.AddAutoMapper(typeof(RoleAutoMapperProfile));
         service.AddAutoMapper(typeof(AccountProfile));
         #endregion
     }
--- Object18/Controllers/HomeController.cs
+++ Object18/Controllers/HomeController.cs
@@ -1,44 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Object18.Models;
 using System.Diagnostics;
-using System.Reflection;
-using Core.Modules.TestModule.Dtos;
-using Core.Modules.TestModule.Services;
-using Microsoft.EntityFrameworkCore;
 
-namespace Object18.Controllers
+namespace Object18.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly ITestService _testService;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger, ITestService testService)
-        {
-            _logger = logger;
-            _testService = testService;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public async Task<IActionResult> Privacy()
+    {
+        return View();
+    }
 
-        public async Task<IActionResult> Privacy()
-        {
-            var createDto = new RoleCreateDto
-            {
-                RoleDescription = "xxx",
-                RoleTitle = "11"
-            };
-            await _testService.AddTest(createDto);
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
