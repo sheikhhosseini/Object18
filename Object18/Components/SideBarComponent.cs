53ebb4e09ea36252e539f4d312d5c687d24b07c9
mssheikh1377 <mssheikh1377@gmail.com>
2022-07-23 22:41:08 +0430

    #39 فارسی سازی قالب اولیه داشبورد انجام شد

---
+++ Object18/Components/SideBarComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Object18.Components;
+
+public class SideBarComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View("/Views/Components/_SideBarComponent.cshtml");
+    }
+}
