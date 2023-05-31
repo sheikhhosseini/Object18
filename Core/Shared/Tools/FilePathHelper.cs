3165bba45d34ab3312b53db39e21fe02a18c8a59
saeedSecondWin <mssheikh1377@gmail.com>
2023-05-31 10:10:28 +0330

    #48 دریافت لینک عکس به صورت هلپر

---
+++ Core/Shared/Tools/FilePathHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Shared.Tools;
+
+public static class FilePathHelper
+{
+    public static string ImagePath(string imageName)
+    {
+        //return Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot/Images/{imageName}");
+        return $"/Images/{imageName}";
+
+    }
+}
--- Core/Shared/Tools/FileSaver.cs
+++ Core/Shared/Tools/FileSaver.cs
@@ -1,70 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
-using System.IO;
 
 namespace Core.Shared.Tools;
 
 public class FileSaver
 {
-    //public static async Task<string> CreateImage(IFormFile file)
-    //{
-    //    string imageName;
-    //    if (file != null)
-    //    {
-    //        imageName = MyUniqCode.GenerateActiveCode() + Path.GetExtension(file.FileName);
-
-    //        string imagePath = Path.Combine(Directory.GetCurrentDirectory(),
-    //            "wwwroot/Images/UserAvatar", imageName);
-
-    //        await using var stream = new FileStream(imagePath, FileMode.Create);
-    //        await file.CopyToAsync(stream);
-    //    }
-    //    else
-    //    {
-    //        imageName = "Default.jpg";
-    //    }
-    //    return imageName;
-    //}
-
-    //public static async Task<string> UpdateImage(IFormFile file, string oldImage)
-    //{
-    //    string imageName;
-    //    if (file != null)
-    //    {
-    //        string imagePath;
-    //        if (oldImage != "Default.jpg")
-    //        {
-    //            imagePath = Path.Combine(Directory.GetCurrentDirectory(),
-    //                "wwwroot/Images/UserAvatar", oldImage);
-    //            if (File.Exists(imagePath))
-    //            {
-    //                File.Delete(imagePath);
-    //            }
-    //        }
-
-    //        imageName = MyUniqCode.GenerateActiveCode() + Path.GetExtension(file.FileName);
-
-    //        imagePath = Path.Combine(Directory.GetCurrentDirectory(),
-    //            "wwwroot/Images/UserAvatar", imageName);
-
-    //        await using var stream = new FileStream(imagePath, FileMode.Create);
-    //        await file.CopyToAsync(stream);
-    //    }
-    //    else
-    //    {
-    //        imageName = oldImage;
-    //    }
-    //    return imageName;
-    //}
-
-
-
     public static async Task<string> CreateImage(IFormFile file, string folderName)
     {
         string imageName = null;
 
         if (file != null)
         {
-            imageName = MyUniqCode.GenerateActiveCode() + Path.GetExtension(file.FileName);
+            imageName = $"{folderName}/{MyUniqCode.GenerateActiveCode() + Path.GetExtension(file.FileName)}";
 
             string folderPath = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot/Images/{folderName}");
             if (!Directory.Exists(folderPath))
@@ -72,7 +18,7 @@ public class FileSaver
                 Directory.CreateDirectory(folderPath);
             }
 
-            string imagePath = Path.Combine(folderPath, imageName);
+            string imagePath = Path.Combine(Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot/Images/{imageName}"));
 
             await using var stream = new FileStream(imagePath, FileMode.Create);
             await file.CopyToAsync(stream);
@@ -89,7 +35,7 @@ public class FileSaver
             if (!string.IsNullOrEmpty(oldImage))
             {
                 var imagePath = Path.Combine(Directory.GetCurrentDirectory(),
-                    $"wwwroot/Images/{folderName}", oldImage);
+                    $"wwwroot/Images/{oldImage}");
                 if (File.Exists(imagePath))
                 {
                     File.Delete(imagePath);
